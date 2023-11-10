4aa7b97cedf88c1670229634d7bf98971bd2fcab
habitcoach <the_habit_coach@outlook.com>
2023-11-10 21:18:24 +0530

    Created all base folder.  Create domain project with interface and domain models.

---
+++ CleanDomain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanDomain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProduct();
+    }
+}
---
+++ CleanDomain/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanDomain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
