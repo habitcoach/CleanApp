a97126531e0f7c59b79387ec4e1ec02858d8e202
habitcoach <the_habit_coach@outlook.com>
2023-11-10 21:25:52 +0530

    Created application project with necessary interface and service and dto

---
+++ Clean.Application/DTOs/Dto.cs
@@ -0,0 +1,14 @@
+﻿using CleanDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Application.DTOs
+{
+    public class Dto
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ Clean.Application/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Clean.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Dto GetProduct();
+    }
+}
